3b72353a43df408e1f42eda2b030b77b5da06f08
Tadas Majeris <tadas.majeris@gmail.com>
2016-11-08 15:16:44 +0000

    it function and toBeEmpty

--- assert.js
+++ assert.js
@@ -5,15 +5,32 @@ var assert = {
     return this;
   },
 
+  addTestDescription: function(string) {
+    this.testDescription = string;
+  },
+
   toEqual: function(n) {
     if(this.assertArgument === n) {
-      console.log("GREEN: expected '"  + n + "', and got '" + this.assertArgument + "'")
+      console.log(this.testDescription + " - GREEN: expected '"  + n + "', and got '" + this.assertArgument + "'")
     } else {
-      throw new Error("expected '" + n + "', but got '" + this.assertArgument + "'")
+      throw new Error(this.testDescription + " - expected '" + n + "', but got '" + this.assertArgument + "'")
+    }
+  },
+
+  toBeEmpty: function() {
+    if(this.assertArgument.length === 0) {
+      console.log(this.testDescription + " - GREEN: expected '"  + this.assertArgument + "' to be empty, and it was!")
+    } else {
+      throw new Error(this.testDescription + " - expected '" + this.assertArgument + "' to be empty.")
     }
   }
 }
 
 var expect = function(argument) {
   return assert.addArgument(argument);
+}
+
+var it = function(description, funct){
+  assert.addTestDescription(description);
+  func();
 }
