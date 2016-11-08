45800421a93115afaa0ddab81809933ca63b69da
Tadas Majeris <tadas.majeris@gmail.com>
2016-11-08 15:33:13 +0000

    describe

--- assert.js
+++ assert.js
@@ -5,29 +5,38 @@ var assert = {
     return this;
   },
 
+  addDescription: function(string) {
+    this.description = string + ": ";
+  },
+
   addTestDescription: function(string) {
     this.testDescription = string;
   },
 
   toEqual: function(n) {
     if(this.assertArgument === n) {
-      console.log(this.testDescription + " - GREEN: expected '"  + n + "', and got '" + this.assertArgument + "'")
+      console.log(this.description + this.testDescription + " - GREEN: expected '"  + n + "', and got '" + this.assertArgument + "'")
     } else {
-      throw new Error(this.testDescription + " - expected '" + n + "', but got '" + this.assertArgument + "'")
+      throw new Error(this.description + this.testDescription + " - expected '" + n + "', but got '" + this.assertArgument + "'")
     }
   },
 
   toBeEmpty: function() {
     if(this.assertArgument.length === 0) {
-      console.log(this.testDescription + " - GREEN: expected '"  + this.assertArgument + "' to be empty, and it was!")
+      console.log(this.description + this.testDescription + " - GREEN: expected '"  + this.assertArgument + "' to be empty, and it was!")
     } else {
-      throw new Error(this.testDescription + " - expected '" + this.assertArgument + "' to be empty.")
+      throw new Error(this.description + this.testDescription + " - expected '" + this.assertArgument + "' to be empty.")
     }
   }
 }
 
 var expect = function(argument) {
   return assert.addArgument(argument);
+}
+
+var describe = function(description, funct){
+  assert.addDescription(description);
+  funct();
 }
 
 var it = function(description, funct){
